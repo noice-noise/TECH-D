cc681cca8b5a3ec0dfb8065f2c4481ba0068ad95
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-09-14 13:30:02 +0800

    Update TECH-D

---
+++ Assets/FocusDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusDebug : MonoBehaviour
+{
+    public bool debug;
+    public float lineLength;
+
+    private void OnDrawGizmos() {
+        if (debug) {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, 0, lineLength));
+        }
+    }
+}
