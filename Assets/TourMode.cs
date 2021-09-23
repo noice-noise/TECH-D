beec4814b58b8ba0e71540fece4770a47ccd4518
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-09-23 23:35:25 +0800

    Update TourMode functionality with auto switch and terminate conditions

--- Assets/TourMode.cs
+++ Assets/TourMode.cs
@@ -12,32 +12,48 @@ public class TourMode : MonoBehaviour {
     private Transform currentlySelected;
 
     public bool onTourMode = false;
-    public float duration;
+    public bool autoSwitch = true;
+    public bool onCountdown = false;
+
+    public float duration = 2f;
 
 
     private void Awake() {
         world = GameObject.FindGameObjectWithTag("World");
         tourList = new List<Transform>();
         PopulateTourList();
-        // DisplayTourList();
-    }
-
-    /// <summary>
-    /// Populates the tour list based on the order of GameObjects on "World"
-    /// </summary>
-    private void PopulateTourList() {
-        var list = world.transform.Cast<Transform>().ToList();
-
-        foreach (Transform item in list) {
-            if (item != null && item.CompareTag("SelectableBuilding")) {
-                tourList.Add(item);
-            }
-        }
-
     }
 
     private void Update() {
+        HandleTerminateConditions();
         HandleAllInputs();
+        HandleTourAutoSwitch();
+    }
+
+    private void HandleTerminateConditions() {
+        if (OnMapView()) {
+            StopNextTourTargetCoroutine();
+            onTourMode = false;
+        }
+    }
+
+    private void HandleTourAutoSwitch() {
+        if (onTourMode && autoSwitch && !onCountdown) {
+            StartCoroutine("NextTourTargetCoroutine");
+        }
+    }
+
+    IEnumerator NextTourTargetCoroutine() {
+        onCountdown = true;
+        yield return new WaitForSeconds(duration);
+        IncrementCounter();
+        SelectCurrentTourTarget();
+        onCountdown = false;
+    }
+
+    public void StopNextTourTargetCoroutine() {
+        onCountdown = false;
+        StopCoroutine("NextTourTargetCoroutine");
     }
 
     private void HandleAllInputs() {
@@ -51,23 +67,78 @@ public class TourMode : MonoBehaviour {
     private void HandleTourModeToggle() {
         if (Input.GetKeyDown(KeyCode.T)) {
             onTourMode = !onTourMode;
-            Debug.Log("TourMode:l " + onTourMode);
+
+            if (!onTourMode) {
+                StopNextTourTargetCoroutine();
+            } else if (onTourMode) {
+                CameraManager.Instance.SwitchCameraMode(CameraManager.CameraState.FocusedView);
+                SelectCurrentTourTarget();
+            }
         }
     }
 
     private void HandleTourHotkeys() {
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            if (tourCounter > 1) {
-                tourCounter--;
-            }
+            DecrementCounter();
+            SelectCurrentTourTarget();
         } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            if (tourCounter < tourList.Count() - 1) {
-                
-                tourCounter++;
+            IncrementCounter();
+            SelectCurrentTourTarget();
+        }
+    }
+
+    private void SelectCurrentTourTarget() {
+        currentlySelected = tourList[tourCounter];
+        Navigation.Instance.SelectAndUpdateUI(currentlySelected);
+        Debug.Log(currentlySelected.name + " " + tourCounter.ToString());
+    }
+
+    /// <summary>
+    /// Populates the tour list based on the order of GameObjects on "World"
+    /// </summary>
+    private void PopulateTourList() {
+        var list = world.transform.Cast<Transform>().ToList();
+
+        foreach (Transform item in list) {
+            if (item != null && item.CompareTag("SelectableBuilding")) {
+                tourList.Add(item);
             }
         }
+    }
 
-        Navigation.Instance.SelectAndUpdateUI(tourList[tourCounter]);
+    private void IncrementCounter() {
+        int tourListLength = tourList.Count();
+
+        if (tourCounter < tourListLength) {
+            tourCounter++;
+        } 
+
+        if (tourCounter >= tourListLength) {
+            ResetCounter();
+        }
+    }
+
+    private void ResetCounter() {
+        tourCounter = 0;
+    }
+
+    private void DecrementCounter() {
+        if (tourCounter > 0) {
+            tourCounter--;
+        }
+
+        if (tourCounter <= 0) {
+            MaxCounter();
+        }
+    }
+
+    private void MaxCounter() {
+        tourCounter = tourList.Count() - 1;
+    }
+
+
+    private bool OnMapView() {
+        return CameraManager.Instance.currentCameraState == CameraManager.CameraState.MapView;
     }
 
     private void DisplayTourList() {
