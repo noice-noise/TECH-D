87e42b6bdeb444046d4be2545a767406d4797d63
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-09-24 20:01:42 +0800

    Add queries data

---
+++ Assets/Scripts/Queries/QueriesData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QueriesData", menuName = "TECH-D/QueriesData", order = 0)]
+public class QueriesData : ScriptableObject {
+    public List<Category> categories;
+}
+
+[System.Serializable]
+public class Category {
+    public string title;
+    public List<Query> queries;
+}
+
+[System.Serializable]
+public class Query {
+    public string title;
+    public List<Section> sections;
+}
+
+[System.Serializable]
+public class Section {
+    public string title;
+    [TextArea]
+    public string contents;
+}
