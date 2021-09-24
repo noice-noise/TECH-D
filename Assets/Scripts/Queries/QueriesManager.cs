797cd4e7e5bea196f83e14e526ce4944ebf66256
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-09-24 20:27:04 +0800

    Update queries feature

---
+++ Assets/Scripts/Queries/QueriesManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QueriesManager : MonoBehaviour {
+    public QueriesData queriesData;
+    public GameObject queryCategoriesPrefab;
+
+    public Transform categoryPane;
+
+    private void Awake() {
+        InitCategory();
+    }
+
+    private void InitCategory() {
+        var categoryList = categoryPane.Find("List");
+        if (categoryList == null) {
+            Debug.LogError("List in categoryPane not found.");
+            return;
+        }
+
+        foreach (Category category in queriesData.categories) {
+            CreateCategory(category, categoryList, queryCategoriesPrefab);
+        }
+    }
+
+    private void CreateCategory(Category categoryInfo, Transform targetParent, GameObject prefab) {
+
+        GameObject newButton = Instantiate(prefab, targetParent, false);
+        newButton.transform.SetParent(targetParent);
+        newButton.transform.name = categoryInfo.title;
+        newButton.transform
+            .Find("Text")
+            .GetComponent<TextMeshProUGUI>()
+            .SetText(categoryInfo.title);
+
+
+        newButton.GetComponent<Button>().onClick.AddListener( 
+            delegate { OnCategoryClicked(newButton.transform); });
+    }
+
+    public void OnCategoryClicked(Transform source) {
+        Debug.Log(source.name);
+    }
+}
