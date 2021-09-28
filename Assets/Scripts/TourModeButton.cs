7050a90fd8f7dfc1166233803525dc297c016d1b
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-09-28 19:45:57 +0800

    Add environment assets

---
+++ Assets/Scripts/TourModeButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TourModeButton : MonoBehaviour{
+
+    public GameObject tourModeObject;
+    private TourMode tourMode;
+    [SerializeField] private bool onTourMode;
+
+    private Button button;
+    private ColorBlock defaultColor;
+
+    private void Awake() {
+        tourMode =  tourModeObject.GetComponent<TourMode>();
+        button = GetComponent<Button>();
+        defaultColor = button.colors;
+    }
+
+    private void Update() {
+        onTourMode = tourMode.onTourMode;
+        HandleButtonState();
+    }
+
+    private void HandleButtonState() {
+        if (onTourMode) {
+            ChangeNormalColor();
+        } else {
+            RestoreColor();
+        }
+    }
+
+    private void RestoreColor() {
+        button.colors = defaultColor; 
+    }
+
+    private void ChangeNormalColor() {
+        ColorBlock colorVal =  button.colors;
+        colorVal.normalColor = colorVal.highlightedColor;
+        button.colors = colorVal;
+    }
+} 
