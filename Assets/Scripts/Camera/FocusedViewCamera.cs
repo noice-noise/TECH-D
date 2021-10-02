ab62f6810ed8236251d0d047e0063020989d85bf
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-02 21:24:05 +0800

    Update FocusedViewCamera.cs

--- Assets/Scripts/Camera/FocusedViewCamera.cs
+++ Assets/Scripts/Camera/FocusedViewCamera.cs
@@ -4,8 +4,11 @@ using System;
 
 public class FocusedViewCamera : MonoBehaviour {
 
-    public Vector3 focusedViewOffset = Vector3.zero;
-    public Vector3 baseOffset;
+    public Vector3 adjustedFollowOffset = Vector3.zero;
+    public Vector3 baseFollowOffset;
+
+    public Vector3 adjustedTrackedObjectOffset = Vector3.zero;
+    public Vector3 baseTrackedObjectOffset;
 
     private CinemachineVirtualCamera focusedViewCamera;
     private Transform currentTarget;
@@ -13,7 +16,8 @@ public class FocusedViewCamera : MonoBehaviour {
 
     private void Awake() {
         focusedViewCamera = GetComponent<CinemachineVirtualCamera>();
-        baseOffset =  focusedViewCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+        baseFollowOffset =  focusedViewCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+        baseTrackedObjectOffset =  focusedViewCamera.GetCinemachineComponent<CinemachineComposer>().m_TrackedObjectOffset;
     }
 
     private void Update() {
@@ -26,21 +30,35 @@ public class FocusedViewCamera : MonoBehaviour {
 
     private void HandleFocusedOffset() {
         if (currentTarget != null && !currentTarget.CompareTag("FocusedViewTarget")) {
-            AdjustAimOffset(focusedViewCamera, focusedViewOffset);
+            AdjustAimOffset(focusedViewCamera, adjustedFollowOffset);
+            AdjustTrackedOffset(focusedViewCamera, adjustedTrackedObjectOffset);
         } else {
-            RestoreAimOffset(focusedViewCamera, baseOffset);
+            RestoreAimOffset(focusedViewCamera, baseFollowOffset);
+            RestoreTrackedOffset(focusedViewCamera, baseTrackedObjectOffset);
         }
     }
 
     private void AdjustAimOffset(CinemachineVirtualCamera camera, Vector3 offset) {
         Debug.Log("Offset adjusted");
         var camTransposer = camera.GetCinemachineComponent<CinemachineTransposer>();
-        camTransposer.m_FollowOffset = baseOffset + offset;
+        camTransposer.m_FollowOffset = offset;
     }
 
     private void RestoreAimOffset(CinemachineVirtualCamera camera, Vector3 offset) {
         Debug.Log("Offset restored");
         var camTransposer = camera.GetCinemachineComponent<CinemachineTransposer>();
-        camTransposer.m_FollowOffset = baseOffset;
+        camTransposer.m_FollowOffset = offset;
+    }
+
+    private void AdjustTrackedOffset(CinemachineVirtualCamera camera, Vector3 offset) {
+        Debug.Log("Offset adjusted");
+        var camComposer = camera.GetCinemachineComponent<CinemachineComposer>();
+        camComposer.m_TrackedObjectOffset = offset;
+    }
+
+    private void RestoreTrackedOffset(CinemachineVirtualCamera camera, Vector3 offset) {
+        Debug.Log("Offset restored");
+        var camComposer = camera.GetCinemachineComponent<CinemachineComposer>();
+        camComposer.m_TrackedObjectOffset = offset;
     }
 }
