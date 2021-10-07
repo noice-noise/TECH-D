bb924e17a76a7453b5b120177f360621a2cb386a
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-07 11:18:23 +0800

    Add input handler for agent follow

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Settings : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Settings : MonoBehaviour {
+
+    public Transform navMeshAgent;
+
+    private void Update() {
+        HandleKeyInputs();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void HandleKeyInputs() {
+        if (Input.GetKeyDown(KeyCode.F12)) {
+            CameraManager.Instance.SwitchCameraMode(CameraManager.CameraState.TopView);
+            CameraManager.Instance.SwitchTopViewTarget(navMeshAgent);
+        }
     }
 }
