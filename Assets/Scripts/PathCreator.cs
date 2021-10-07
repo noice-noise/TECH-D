55db87b8c6afaae0b8f24a68ccb4cb0eef52f37c
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-07 08:46:29 +0800

    Organize scripts

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour {
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public LineRenderer lineRenderer;
+
+    private void Start() {
+        // DrawPath();
+    }
+
+    // private void DrawPath() {
+    //     throw new NotImplementedException();
+    // }
+}
