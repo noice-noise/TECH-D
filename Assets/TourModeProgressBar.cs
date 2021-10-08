7f2f116f358b679e8f93e0b9df7fcff3a271e834
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-08 16:03:56 +0800

    Add tour mode progress bar for auto switch

---
+++ Assets/TourModeProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class TourModeProgressBar : MonoBehaviour {
+
+    private Slider slider;
+
+    private void Awake() {
+        slider = transform.Find("Progress Bar").GetComponent<Slider>();
+    }
+
+    private void Update() {
+        if (TourMode.Instance.onTourMode) {
+            slider.value = TourMode.Instance.countdownPercent;
+        }
+    }
+}
