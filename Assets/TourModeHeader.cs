eea0f06786ec49f94454bde889529bf1a10e1025
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-08 16:07:15 +0800

    Add tour mode header

---
+++ Assets/TourModeHeader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TourModeHeader : MonoBehaviour {
+
+    private TextMeshProUGUI text;
+
+    private void Awake() {
+        var textTrans = transform.Find("Text");
+
+        if (textTrans == null) {
+            Debug.LogError("TextMeshProUGUI text not set.");
+        }
+
+        text = textTrans.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update() {
+        if (TourMode.Instance.onTourMode) {
+            HandleHeaderUpdate();
+        }
+    }
+
+    private void HandleHeaderUpdate() {
+        var target = UIManager.Instance.currentlySelectedBuilding;
+        if (target == null) return;
+        Debug.Log("NN: " + target.name);
+        text.text = target.name;
+    }
+}
