aad473904e0125ab1560cb418c629c932fa135b2
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-09 11:55:41 +0800

    Fix navmesh not baking in Build

---
+++ Assets/NavMeshBaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    public NavMeshSurface navMeshSurface;
+    public NavMeshData navMeshData;
+
+    private void Start() {
+        navMeshSurface.BuildNavMesh();
+        
+        // // navMeshSurface.navMeshData.
+
+        if (navMeshSurface.navMeshData != navMeshData) {
+            navMeshSurface.UpdateNavMesh(navMeshData);
+        }
+    }
+}
