02a17292a4166329a3212f87a64a491e01de23b2
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-14 22:50:00 +0800

    Add start view

--- Assets/Scripts/Camera/SystemManager.cs
+++ Assets/Scripts/Camera/SystemManager.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SystemManager : Singleton<SystemManager> {
 
+    public GameObject startObject;
+
+    private void Update() {
+        SelectStartingView();
+    }
+
+    private void SelectStartingView() {
+        Navigation.Instance.SelectAndUpdateUI(startObject.transform);
+    }
 
     public void RestartSystem() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
