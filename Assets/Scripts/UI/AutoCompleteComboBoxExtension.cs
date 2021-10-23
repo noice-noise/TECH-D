1681f76b135ac0263080173769dd714f3ea77f6c
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-10-23 12:24:14 +0800

    Fix search not showing suggestions initially and the second time

--- Assets/Scripts/UI/AutoCompleteComboBoxExtension.cs
+++ Assets/Scripts/UI/AutoCompleteComboBoxExtension.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UI.Extensions;
 
 public class AutoCompleteComboBoxExtension : MonoBehaviour {
+    
+    public AutoCompleteComboBox search;
+    public InputField inputField;
+    public GameObject suggestionOverlay;
+    
+    public Button clearButton;
+
+    private void OnEnable() {
+        ShowSuggestionPanel();
+    }    
+
     private void Start() {
-        // show drop down panel
-        GetComponent<AutoCompleteComboBox>().ToggleDropdownPanel(false);
+        clearButton.onClick.AddListener(delegate { ShowSuggestionPanel(); });
+    }
+
+    private void Update() {
+        HandleFocused();
+    }
+
+    private void HandleFocused() {
+        if (inputField.isFocused) {
+            HandleEmptyInputField();
+        }
+    }
+
+    private void HandleEmptyInputField() {
+        ShowSuggestionPanel();
+    }
+
+    private void ShowSuggestionPanel() {
+        if (!suggestionOverlay.activeSelf) {
+            search.ToggleDropdownPanel(false);
+        }
     }
 }
